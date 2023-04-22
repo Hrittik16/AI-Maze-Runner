afa8c99e59ee1fa51d48fc48552d28dddf12f293
Hrittik Das <hrittikdas16@gmail.com>
2023-04-22 20:21:35 +0530

    updated

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from maze_generator import generate_maze
-from maze_generator import MazeGUI
+from maze_generator import generate_maze, MazeGUI
 
 maze = generate_maze(20, 20, 50)  # generate_maze(rows, columns, num_walls)
 maze_gui = MazeGUI(maze)
